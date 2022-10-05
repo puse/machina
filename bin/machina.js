7e52b0f4a2b5233e28dc70e1f02c6cf9a3f3d6ee
Mushegh Antaranyan <mushegh.codes@gmail.com>
2022-10-05 17:15:21 +0400

    Show CLI help when no command argument

--- bin/machina.js
+++ bin/machina.js
@@ -4,4 +4,18 @@ const { run } = require("../build/cli/cli"); // eslint-disable-line @typescript-
 
 const { _: input, ...flags } = parseArgs(process.argv.slice(2));
 
+const helpText = `
+	Usage
+	  $ machina <command> <input>
+	Options
+	  --headless  Run browser in headless mode
+	Examples
+	  $ machina tweets shakira
+	  $ machina details shakira --no-headless
+`;
+if (input.length === 0) {
+  console.log(helpText);
+  process.exit(1);
+}
+
 run(input, flags);
